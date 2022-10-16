e8caf8ebad12e024461b672e75d34b19d1b4755f
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-10-16 23:25:34 +1000

    info

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from './components/app/App';
 
 import './style/style.scss';
 
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,31 +1,37 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=0f36da5d5e82d0288523df1ce9e457cb';
-    getResurce = async (url) => {
-        let res = await fetch(url);
+    // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+    _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62';
 
+    getResource = async (url) => {
+        let res = await fetch(url);
+    
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-
+    
         return await res.json();
     }
+
     getAllCharacters = async () => {
-        const res = await this.getResurce(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
+
     getCharacter = async (id) => {
-        const res = await this.getResurce(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
         return this._transformCharacter(res.data.results[0]);
     }
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
